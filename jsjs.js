c3c2958f3bf6ad58dbf7aa62e15d8af2cfcb9ce6
Nathanael Bonamie <nathbonamie@orange.fr>
2023-04-10 10:57:29 +0200

    Add files via upload

---
+++ jsjs.js
@@ -0,0 +1,54 @@
+var tag;
+var comm;
+var tst = false;
+var url = ("https://wol.jw.org/fr/wol/h/r30/lp-f/2023/4/8");
+const xhttp = new XMLHttpRequest();
+	xhttp.onload = function() {
+    tag = this.responseText;
+var parser = new DOMParser();
+var doc = parser.parseFromString(tag, "text/html");
+var id = doc.getElementsByTagName("h2")[1].getAttribute("id").replace(/\D+/, "");
+
+var jour = doc.getElementById("p"+id).innerText;
+var ref = doc.getElementById("p"+(parseInt(id)+1)).innerText;
+var com = doc.getElementById("p"+(parseInt(id)+2)).innerText;
+
+var refText = ref.substring(0, ref.lastIndexOf('('));
+var refTextt = ref.substring(ref.lastIndexOf('('));
+
+
+refTextt = refTextt.replace(":", " verset ");
+refText += refTextt;
+
+com = extractWords(com);
+
+console.log(jour + "\n\n" + refText + "\n\n" + com);
+
+var blob = new Blob([jour + "\n\n" + refText + "\n\n" + com], {type: "text/plain;charset=utf-8"});
+saveAs(blob, "helloworld.txt");
+
+  }
+xhttp.open("GET", url, true);
+xhttp.send();
+  
+function extractWords(str) {
+	str = [...str];
+	var i=0
+    for (i; i < str.length; i++) {
+        if(str[i] === '(') {
+            tst = true;
+        }
+		if(tst & (str[i] === ':')){
+			str[i] = ' verset ';
+		}
+		else if(tst & (str[i] === ',')){
+			str[i] = ' et';
+		}
+		else if(tst & (str[i] === '-')){
+			str[i] = ' a ';
+		}
+		if(str[i] === ')')
+			tst = false;
+    }
+    return str.join('');
+}
